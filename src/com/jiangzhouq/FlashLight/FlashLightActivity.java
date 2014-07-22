dbc7c3994c36442dd5eca2c8b302aa2be3a02015
jiangzhouq <jiangzhouq@gmail.com>
2014-07-22 16:28:07 +0800

    rhythm按键逻辑&&music1&&music2

--- src/com/jiangzhouq/FlashLight/FlashLightActivity.java
+++ src/com/jiangzhouq/FlashLight/FlashLightActivity.java
@@ -1,6 +1,9 @@
 package com.jiangzhouq.FlashLight;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -33,12 +36,26 @@ public class FlashLightActivity extends Activity implements OnClickListener{
 	private RelativeLayout bottom_relative;
 	private ImageView top_image;
 	
-	private int[] rhythm_buttons = new int[]{R.id.flicker_grade_1,
+	private int[] flicker_buttons = new int[]{
+			R.id.flicker_grade_1,
 			R.id.flicker_grade_2,
 			R.id.flicker_grade_3,
 			R.id.flicker_grade_4,
 			R.id.flicker_grade_5};
-	
+	private int[] rhythm_buttons = new int[]{
+			R.id.rhythm_grade_1,
+			R.id.rhythm_grade_2,
+			R.id.rhythm_grade_3,
+			R.id.rhythm_grade_4,
+			R.id.rhythm_grade_5,
+			R.id.rhythm_grade_6,
+			R.id.rhythm_grade_7,
+			R.id.rhythm_grade_8,
+			R.id.rhythm_grade_9,
+			R.id.rhythm_grade_10,
+			R.id.rhythm_grade_11,
+			R.id.rhythm_grade_12
+	};
 	Handler mHandler = new  Handler(){
 	};
 	/** Called when the activity is first created. */
@@ -88,7 +105,7 @@ public class FlashLightActivity extends Activity implements OnClickListener{
             }
         }
     };
-    private int defaultRate[] = new int[]{800,200,800,200,300,200,300,200,300,1000};
+    private int defaultRate[] = new int[]{1000,200,1000,200,400,200,400,200,1000,200};
     private int rate_num = 0;
     
     class RhythmRun implements Runnable{
@@ -105,20 +122,44 @@ public class FlashLightActivity extends Activity implements OnClickListener{
         }
         public void setRate(int rate){
             switch(rate){
+            	//music1
                 case 1:
-                    defaultRate = new int[]{300,300,150,150,150};
+                    defaultRate = new int[]{100,100,100,1000,100,1000};
                     break;
                 case 2:
-                    defaultRate = new int[]{300,300,150,150,150};
+                    defaultRate = new int[]{100,100,100,800,100,800};
                     break;
                 case 3:
-                    defaultRate = new int[]{300,300,150,150,150};
+                    defaultRate = new int[]{100,100,100,500,100,500};
                     break;
                 case 4:
-                    defaultRate = new int[]{300,300,150,150,150};
+                    defaultRate = new int[]{100,100,100,300,100,300};
                     break;
+                //music2
                 case 5:
-                    defaultRate = new int[]{300,300,150,150,150};
+                    defaultRate = new int[]{100,1000,100,1000,100,300,100,300,100,1000};
+                    break;
+                case 6:
+                    defaultRate = new int[]{100,800,100,800,100,200,100,200,100,800};
+                    break;
+                case 7:
+                    defaultRate = new int[]{100,500,100,500,100,100,100,100,100,500};
+                    break;
+                case 8:
+                    defaultRate = new int[]{100,300,100,300,100,100,100,100,100,300};
+                    break;
+                //music3
+                case 9:
+                    defaultRate = new int[]{100,200,100,100,100,100,100,200,100,200,100,100,100,100,100,100,100,100,100,200};
+                    break;
+                case 10:
+                    defaultRate = new int[]{800,200,800,200,300,200,300,200,800,200};
+                    break;
+                case 11:
+                    defaultRate = new int[]{600,150,600,150,300,150,300,150,600,150};
+                    break;
+                case 12:
+                    defaultRate = new int[]{400,150,400,150,200,150,200,150,400,150};
                     break;
             }
         }
@@ -141,10 +182,10 @@ public class FlashLightActivity extends Activity implements OnClickListener{
 			}
 			break;
 		case 1:
-			mHandler.post(flickerRun);
+			mHandler.postDelayed(flickerRun,500);
 			break;
 		case 2:
-			mHandler.post(rhythmRun);
+			mHandler.postDelayed(rhythmRun,500);
 			break;
 		case 3:
 			break;
@@ -190,55 +231,102 @@ public class FlashLightActivity extends Activity implements OnClickListener{
                 }
                 break;
             case R.id.flicker_grade_1:
-            	flickerRun.setRate(1);
-            	recoverFlickerButtons();
-            	((RelativeLayout) findViewById(R.id.flicker_grade_1)).setBackgroundResource(R.drawable.mode_button_rhythm_pressed);
-            	((TextView)((RelativeLayout) findViewById(R.id.flicker_grade_1)).getChildAt(0)).setTextColor(getResources().getColor(R.color.my_blue));
+            	setFlickerRate(0);
             	break;
             case R.id.flicker_grade_2:
-            	flickerRun.setRate(2);
-            	recoverFlickerButtons();
-            	((RelativeLayout) findViewById(R.id.flicker_grade_2)).setBackgroundResource(R.drawable.mode_button_rhythm_pressed);
-            	((TextView)((RelativeLayout) findViewById(R.id.flicker_grade_2)).getChildAt(0)).setTextColor(getResources().getColor(R.color.my_blue));
+            	setFlickerRate(1);
             	break;
             case R.id.flicker_grade_3:
-            	flickerRun.setRate(3);
-            	recoverFlickerButtons();
-            	((RelativeLayout) findViewById(R.id.flicker_grade_3)).setBackgroundResource(R.drawable.mode_button_rhythm_pressed);
-            	((TextView)((RelativeLayout) findViewById(R.id.flicker_grade_3)).getChildAt(0)).setTextColor(getResources().getColor(R.color.my_blue));
+            	setFlickerRate(2);
             	break;
             case R.id.flicker_grade_4:
-            	flickerRun.setRate(4);
-            	recoverFlickerButtons();
-            	((RelativeLayout) findViewById(R.id.flicker_grade_4)).setBackgroundResource(R.drawable.mode_button_rhythm_pressed);
-            	((TextView)((RelativeLayout) findViewById(R.id.flicker_grade_4)).getChildAt(0)).setTextColor(getResources().getColor(R.color.my_blue));
+            	setFlickerRate(3);
             	break;
             case R.id.flicker_grade_5:
-            	flickerRun.setRate(5);
-            	recoverFlickerButtons();
-            	((RelativeLayout) findViewById(R.id.flicker_grade_5)).setBackgroundResource(R.drawable.mode_button_rhythm_pressed);
-            	((TextView)((RelativeLayout) findViewById(R.id.flicker_grade_5)).getChildAt(0)).setTextColor(getResources().getColor(R.color.my_blue));
+            	setFlickerRate(4);
             	break;
-//			if(isFlashOn){
-//            stopFlash();
-//            isFlashOn = false;
-//			}else{
-//				startFlash(!mSwitch.isChecked());
-//				isFlashOn = true;
-//			}
+            case R.id.rhythm_grade_1:
+            	setRhythmRate(0);
+            	break;
+            case R.id.rhythm_grade_2:
+            	setRhythmRate(1);
+            	break;
+            case R.id.rhythm_grade_3:
+            	setRhythmRate(2);
+            	break;
+            case R.id.rhythm_grade_4:
+            	setRhythmRate(3);
+            	break;
+            case R.id.rhythm_grade_5:
+            	setRhythmRate(4);
+            	break;
+            case R.id.rhythm_grade_6:
+            	setRhythmRate(5);
+            	break;
+            case R.id.rhythm_grade_7:
+            	setRhythmRate(6);
+            	break;
+            case R.id.rhythm_grade_8:
+            	setRhythmRate(7);
+            	break;
+            case R.id.rhythm_grade_9:
+            	setRhythmRate(8);
+            	break;
+            case R.id.rhythm_grade_10:
+            	setRhythmRate(9);
+            	break;
+            case R.id.rhythm_grade_11:
+            	setRhythmRate(10);
+            	break;
+            case R.id.rhythm_grade_12:
+            	setRhythmRate(11);
+            	break;
+            	
 		}
 	}
+	private void setFlickerRate(int rate){
+		stopFlash();
+		flickerRun.setRate(rate + 1);
+		recoverFlickerButtons();
+    	((RelativeLayout) findViewById(flicker_buttons[rate])).setBackgroundResource(R.drawable.mode_button_rhythm_pressed);
+    	((TextView)((RelativeLayout) findViewById(flicker_buttons[rate])).getChildAt(0)).setTextColor(getResources().getColor(R.color.my_blue));
+		SharedPreferences sharedPreferences = getSharedPreferences("qjizho", Context.MODE_PRIVATE);
+		Editor editor = sharedPreferences.edit();
+		editor.putInt("flicker_grade", rate);
+		editor.commit();
+		startFlash(1);
+	}
+	private void setRhythmRate(int rate){
+		stopFlash();
+		rhythmRun.setRate(rate + 1);
+		recoverRhythmButtons();
+		((RelativeLayout) findViewById(rhythm_buttons[rate])).setBackgroundResource(R.drawable.mode_button_rhythm_pressed);
+		((TextView)((RelativeLayout) findViewById(rhythm_buttons[rate])).getChildAt(0)).setTextColor(getResources().getColor(R.color.my_blue));
+		SharedPreferences sharedPreferences = getSharedPreferences("qjizho", Context.MODE_PRIVATE);
+		Editor editor = sharedPreferences.edit();
+		editor.putInt("rhythm_grade", rate);
+		editor.commit();
+		startFlash(2);
+	}
 	private void recoverFlickerButtons(){
-		for(int i : rhythm_buttons){
+		for(int i : flicker_buttons){
     		RelativeLayout relative = (RelativeLayout) findViewById(i);
     		relative.setBackgroundResource(R.drawable.mode_button_rhythm);
     		TextView text = (TextView) relative.getChildAt(0);
     		text.setTextColor(getResources().getColor(R.color.my_dark));
     	}
 	}
+	private void recoverRhythmButtons(){
+		for(int i : rhythm_buttons){
+			RelativeLayout relative = (RelativeLayout)findViewById(i);
+			relative.setBackgroundResource(R.drawable.mode_button_rhythm);
+			TextView text = (TextView) relative.getChildAt(0);
+			text.setTextColor(getResources().getColor(R.color.my_dark));
+ 		}
+	}
     private void changeState(int state){
         recoverState();
-        
+        SharedPreferences sharedPreferences = getSharedPreferences("qjizho", Context.MODE_PRIVATE);
         switch(state){
             case mState_constant:
                 startFlash(0);
@@ -250,7 +338,6 @@ public class FlashLightActivity extends Activity implements OnClickListener{
                 mConstantsButton.setImageResource(R.drawable.bt_bg_2);
                 break;
             case mState_flicker:
-                startFlash(1);
                 mButton_flicker.setBackgroundResource(R.drawable.mode_button_middle_pressed);
                 bottom_relative.removeAllViews();
                 View.inflate(this,R.layout.flicker_table,bottom_relative);
@@ -258,15 +345,19 @@ public class FlashLightActivity extends Activity implements OnClickListener{
                 ImageButton mFlickerButton = (ImageButton) findViewById(R.id.flicker);
                 mFlickerButton.setImageResource(R.drawable.bt_bg_4);
                 setFlickerListener();
+                int flicker_grade = sharedPreferences.getInt("flicker_grade", 0);
+                setFlickerRate(flicker_grade);
                 break;
             case mState_rhythm:
-            	startFlash(2);
                 mButton_rhythm.setBackgroundResource(R.drawable.mode_button_middle_pressed);
                 bottom_relative.removeAllViews();
                 View.inflate(this,R.layout.rhythm_table,bottom_relative);
                 top_image.setImageResource(R.drawable.top_bg_4);
                 ImageButton mRhythmButton = (ImageButton) findViewById(R.id.rhythm);
                 mRhythmButton.setImageResource(R.drawable.bt_bg_6);
+                setRhythmListener();
+                int rhythm_grade = sharedPreferences.getInt("rhythm_grade", 0);
+                setRhythmRate(rhythm_grade);
                 break;
             case mState_slow:
                 mButton_slow.setBackgroundResource(R.drawable.mode_button_right_pressed);
@@ -348,7 +439,32 @@ public class FlashLightActivity extends Activity implements OnClickListener{
 		RelativeLayout layout5 = (RelativeLayout) findViewById(R.id.flicker_grade_5);
 		layout5.setOnClickListener(this);
 	}
-	
+	private void setRhythmListener(){
+		RelativeLayout layout1 = (RelativeLayout) findViewById(R.id.rhythm_grade_1);
+		layout1.setOnClickListener(this);
+		RelativeLayout layout2 = (RelativeLayout) findViewById(R.id.rhythm_grade_2);
+		layout2.setOnClickListener(this);
+		RelativeLayout layout3 = (RelativeLayout) findViewById(R.id.rhythm_grade_3);
+		layout3.setOnClickListener(this);
+		RelativeLayout layout4 = (RelativeLayout) findViewById(R.id.rhythm_grade_4);
+		layout4.setOnClickListener(this);
+		RelativeLayout layout5 = (RelativeLayout) findViewById(R.id.rhythm_grade_5);
+		layout5.setOnClickListener(this);
+		RelativeLayout layout6 = (RelativeLayout) findViewById(R.id.rhythm_grade_6);
+		layout6.setOnClickListener(this);
+		RelativeLayout layout7 = (RelativeLayout) findViewById(R.id.rhythm_grade_7);
+		layout7.setOnClickListener(this);
+		RelativeLayout layout8 = (RelativeLayout) findViewById(R.id.rhythm_grade_8);
+		layout8.setOnClickListener(this);
+		RelativeLayout layout9 = (RelativeLayout) findViewById(R.id.rhythm_grade_9);
+		layout9.setOnClickListener(this);
+		RelativeLayout layout10 = (RelativeLayout) findViewById(R.id.rhythm_grade_10);
+		layout10.setOnClickListener(this);
+		RelativeLayout layout11 = (RelativeLayout) findViewById(R.id.rhythm_grade_11);
+		layout11.setOnClickListener(this);
+		RelativeLayout layout12 = (RelativeLayout) findViewById(R.id.rhythm_grade_12);
+		layout12.setOnClickListener(this);
+	}
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
